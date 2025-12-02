f2d19b60d49050c7ec0e6a91a77f903cb420e485
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2025-12-02 21:31:18 +0100

    lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -2,20 +2,50 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+-- local lspconfig = require("nvchad.configs.lspconfig")
 
-lspconfig.servers = {
+local servers = {
     "lua_ls",
     "ts_ls",
-    "c_sharp"
+    "csharp_ls"
 }
 
-local default_servers = {}
+vim.lsp.enable(servers)
 
-for _, lsp in ipairs(default_servers) do
-    lspconfig[lsp].setup({
-        on_attach = on_attach,
-        on_init = on_init,
-        capabilities = capabilities,
-    })
-end
+vim.lsp.config("ts_ls", {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+})
+
+vim.lsp.config("csharp_ls", {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+})
+
+vim.lsp.config("lua_ls", { -- nvim 0.11
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+
+    settings = {
+        Lua = {
+            diagnostics = {
+                enable = false, -- Disable all diagnostics from lua_ls
+                -- globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    vim.fn.expand("$VIMRUNTIME/lua"),
+                    vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+                    vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+                    vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+                    "${3rd}/love2d/library",
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+        },
+    },
+})
